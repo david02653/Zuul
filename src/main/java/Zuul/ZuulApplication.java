a211383fdbf9ab1601fa00216f4cb0a268ebea72
Chen <i11007020@gmail.com>
2019-05-08 02:47:35 +0800

    update ZuulApplication

--- src/main/java/Zuul/ZuulApplication.java
+++ src/main/java/Zuul/ZuulApplication.java
@@ -24,13 +24,9 @@ public class ZuulApplication {
 	    config.setAllowCredentials(true);
 	    config.addAllowedOrigin("*");
 	    config.addAllowedHeader("*");
-	    config.addAllowedMethod("OPTIONS");
 	    config.addAllowedMethod("HEAD");
 	    config.addAllowedMethod("GET");
-	    config.addAllowedMethod("PUT");
 	    config.addAllowedMethod("POST");
-	    config.addAllowedMethod("DELETE");
-	    config.addAllowedMethod("PATCH");
 	    source.registerCorsConfiguration("/**", config);
 	    return new CorsFilter(source);
 	}
