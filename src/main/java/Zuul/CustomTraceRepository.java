19870a92811c82372bb7a216a530d78cf3c06ab7
i11007020 <i11007020@gmail.com>
2019-06-06 07:39:49 +0800

    update properties

---
+++ src/main/java/Zuul/CustomTraceRepository.java
@@ -0,0 +1,27 @@
+package Zuul;
+
+import java.util.Map;
+import java.util.logging.Logger;
+
+import org.springframework.boot.actuate.trace.InMemoryTraceRepository;
+import org.springframework.stereotype.Repository;
+
+import com.google.common.collect.Ordering;
+
+@Repository
+public class CustomTraceRepository extends InMemoryTraceRepository {
+	
+	private static Logger logger = Logger.getLogger(Ordering.class.getName());
+	
+    public CustomTraceRepository() {
+        super.setCapacity(1000);
+    }
+
+    @Override
+    public void add(Map<String, Object> map) {
+        super.add(map);
+        
+        
+        logger.info("traced object: " + map);
+    }
+}
